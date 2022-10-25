a07fbcdea54a3357c637507bfd0fab4b5b9f83e6
avishay avraham <avishayavraham555@gmail.com>
2022-10-26 01:34:16 +0300

    loadingPage

--- client/src/ComponentsFolders/LoadingPage.jsx
+++ client/src/ComponentsFolders/LoadingPage.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useNavigate } from "react-router-dom";
-
+import React from "react";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+import "../style/LoadingPage.css";
 
 export default function LoadingPage() {
-    let navigate = useNavigate();
-    useEffect(() =>{
-        setTimeout(() => {
-            navigate("homePage")
-          }, "5000")
-    },[])
+//   let navigate = useNavigate();
+//   useEffect(() => {
+//     setTimeout(() => {
+//       navigate("homePage");
+//     }, "5000");
+//   }, []);
   return (
-    <div>LoadingPage</div>
-  )
+    
+     <div className="loadingPage">
+      <div id="load">
+        <div>t</div>
+        <div>e</div>
+        <div>N</div>
+        <div>S</div>
+        <div>A</div>
+        <div>L</div>
+        <div>T</div>
+        <div>A</div>
+      
+    </div>
+     </div>
+  );
 }
